cc239fd4d9b1739fb63fb28be3f10bda66fe53b8
imaginedragons <anson.mec@gmail.com>
2023-10-30 18:07:51 +0530

    adding support for statements

--- compiler/main.py
+++ compiler/main.py
@@ -2,10 +2,14 @@
 import argparse
 import os
 from asm_helper import printintinstr, exitinstr, startinstr, addinstr, numlitinstr, divinstr, multinstr, subinstr
-INTEGER ,PLUS, DIV, SUB, MULT, EOF = 'integer', 'plus', 'div', 'subtract','mult', 'eof'
-LPARAN, RPARAN = '(', ')'
-class Token:
+INTEGER ,PLUS, DIV, SUB, MULT, EOF = 'integer', '+', '/', '-','*', 'eof'
+LPARAN, RPARAN, ASSIGN, SEMICOLON = '(', ')', '=', ';'
+DATATYPE = 'datatype'
+IDENTIFIER = 'identifier'
 
+#Define supported datatypes
+DATATYPES = set(['int'])
+class Token:
     def __init__(self, type, value):
         self.type = type
         self.value = value
@@ -16,6 +20,20 @@ class Token:
 class AST:
     pass
 
+class Main:
+    def __init__(self) -> None:
+        self.children = []
+        
+class Identifier:
+    def __init__(self, name_node, type_node, value_node) -> None:
+        self.name = name_node
+        self.type = type_node
+        self.value = value_node
+
+    def __str__(self) -> str:
+        return(f'Token : Name : {self.name} , {self.type}, Value : {self.value}')
+
+        
 class BinOp(AST):
     def __init__(self, left, token, right) -> None:
         super().__init__()
@@ -40,7 +58,7 @@ class Lexer:
     def __init__(self, strcode) -> None:
         self.strcode = strcode
         self.codelen = len(strcode)
-        self.curr_token = None
+        self.identified_str_token = None
         self.curr_pos = 0
         self.curr_char = self.strcode[self.curr_pos]
 
@@ -72,12 +90,27 @@ class Lexer:
             res += self.consume_char()
         return int(res)
             
+    def peek(self):
+        if self.curr_pos < self.codelen:
+            return self.strcode[self.curr_pos + 1]
+        else:
+            return EOF
 
+    def get_next_token_word(self):
+        res = ''
+        while self.curr_char != EOF and self.curr_char.isalpha():
+            res += self.consume_char()
+        return res
 
-    
+    def match(self, tokens_to_check):
+        '''
+        Match a whole token instead of individual chars
+        NOTE: use this function throughout
+        '''
+        pass
+
     def get_next_token(self):
         while self.curr_char != EOF:
-            print(self.curr_char)
             if self.is_whitespace():
                 self.skip_whitespace()
                 continue
@@ -85,30 +118,47 @@ class Lexer:
             if self.curr_char.isdigit():
                 return Token(INTEGER, self.integer())
             
-            if self.curr_char == '+':
-                self.consume_char()
-                return Token(PLUS, '+')
+            if self.curr_char.isalpha():
+                token_word = self.get_next_token_word()
+                if token_word in DATATYPES:
+                    
+                    return Token(DATATYPE ,token_word)
+                else:
+                    print(token_word)
+                    return Token(IDENTIFIER, token_word)
             
-            if self.curr_char == '-':
+            if self.curr_char == PLUS:
                 self.consume_char()
-                return Token(SUB, '-')
+                return Token(PLUS, PLUS)
             
-            if self.curr_char == '*':
+            if self.curr_char == SUB:
                 self.consume_char()
-                return Token(MULT, '*')
+                return Token(SUB, SUB)
             
-            if self.curr_char == '/':
+            if self.curr_char == MULT:
                 self.consume_char()
-                return Token(DIV, '/')
+                return Token(MULT, MULT)
             
-            if self.curr_char == '(':
+            if self.curr_char == DIV:
                 self.consume_char()
-                return Token(LPARAN, '(')
+                return Token(DIV, DIV)
             
-            if self.curr_char == ')':
+            if self.curr_char == LPARAN:
                 self.consume_char()
-                return Token(RPARAN, ')')
+                return Token(LPARAN, LPARAN)
             
+            if self.curr_char == RPARAN:
+                self.consume_char()
+                return Token(RPARAN, RPARAN)
+
+            if self.curr_char == SEMICOLON:
+                self.consume_char()
+                return Token(SEMICOLON, SEMICOLON)
+            
+            if self.curr_char == ASSIGN:
+                self.consume_char()
+                return Token(ASSIGN, ASSIGN)
+
             self.raise_error(f'Unrecognized syntax "{self.curr_char}" at position {self.curr_pos }', exit=1)
 
             
@@ -132,19 +182,49 @@ class Parser:
             self.raise_error(f'Syntax error at position {self.lexer.curr_pos }', exit=1)
         else:
             self.curr_token = self.lexer.get_next_token()
-            # print(self.curr_token)
+
+    def parse(self):
+        stmts =  self.statements()
+        root = Main()
+        for node in stmts:
+            root.children.append(node)
+        return root
+
+    
+    def statements(self):
+        _statements = []
+        # print(self.curr_token)
+        while self.curr_token.type != EOF:
+            if self.curr_token.type == DATATYPE:
+                # print(self.curr_token)
+                _statements.append(self.var_declare())
+        return _statements
+
+    def var_declare(self):
+        curr_tok_dt = self.curr_token
+        self.consume_token(DATATYPE)
+        curr_tok_var = self.curr_token
+        self.consume_token(IDENTIFIER)
+        if self.curr_token.type == ASSIGN:
+            self.consume_token(ASSIGN)
+            node = self.expr()
+            identifier = Identifier(curr_tok_var, curr_tok_dt , node)
+        else:
+            identifier = Identifier(curr_tok_var, curr_tok_dt , None)
+            
+        self.consume_token(SEMICOLON)
+        return identifier
+    
 
     def factor(self):
         if self.curr_token.type == LPARAN:
             self.consume_token(LPARAN)
-            node = self.parse()
+            node = self.expr()
             self.consume_token(RPARAN)
             return node
         else:
             token = self.curr_token
-            # print(token)
             self.consume_token(INTEGER)
-            # print(token.value)
             return NumLiteral(token.value)
 
     def term(self):
@@ -159,8 +239,10 @@ class Parser:
                 # res /= self.factor()
             node = BinOp(left=node, token=tok, right=self.factor())
         return node
+
+    # def prog(self)
     
-    def parse(self):
+    def expr(self):
         """
         expr   : term ((PLUS | SUB) term)*
         term   : factor ((MULT | DIV) factor)* 
@@ -266,15 +348,29 @@ def parse_args():
     parser.add_argument('file', help='File to run')
     parser.add_argument('-sim', dest='sim', action='store_true',help='Simulate it')
     parser.add_argument('-compile', dest='compile', action='store_true', help='Compiile it')
+    parser.add_argument('-debug', dest='debug', action='store_true', help='debug it')
+
     return parser.parse_args()
 
 def main():
     args = parse_args()
     code = open(args.file, 'r').read()
     lexer  = Lexer(code)
+    if args.debug:
+        while 1:
+            x = lexer.get_next_token()
+            print(x)
+            if x.value == EOF:
+                break
+        # return
+    lexer  = Lexer(code)
     # lexer  = Lexer(' (5/2)')
+    # return
     parser = Parser(lexer=lexer)
     root = parser.parse()
+    if args.debug:
+        for child in root.children:
+            print(child)
     if args.sim:
         ir = Interpreter(root=root)
         print(ir.interpret())
@@ -282,13 +378,13 @@ def main():
         compiler = Compile()
         compiler.compile_ast(root)
         os.system('nasm -f elf64 -o hello.o compile.asm && ld -o hello hello.o && ./hello')
+        # os.system('nasm -f macho64 -o hello.o compile.asm && ld -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem -arch x86_64 -macosx_version_min 10.13 -o hello hello.o && ./hello')
     
    
 
 
 if __name__ == '__main__':
     main()
-
 
 
 
