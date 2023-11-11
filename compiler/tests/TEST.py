c6e525da84a71ebee04ea858fc5d7a890a092532
Anson avarachan <anson.mec@gmail.com>
2023-11-11 19:03:07 +0530

    Update TEST.py

--- compiler/tests/TEST.py
+++ compiler/tests/TEST.py
@@ -1,1 +1,6 @@
 
+import os
+os.system('python test1.py > o')
+os.system('python ../main.py test1.an -sim > o1')
+os.system('python ../main.py test1.an -compile > o2')
+print(open('o','r').read() == open('o1','r').read() == open('o2','r').read())
