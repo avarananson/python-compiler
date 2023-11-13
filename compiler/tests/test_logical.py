83c284c155c98d02186fb68664c76cc7e94bfeb1
Anson avarachan <anson.mec@gmail.com>
2023-11-13 13:37:36 +0530

    Create test_logical.py

---
+++ compiler/tests/test_logical.py
@@ -0,0 +1,13 @@
+y,z = 1,1
+x = 0 and 0 and 0;
+k = int((1 and 1) and (0 or 1));
+print(k);
+a = int((y==z) and (1==1));
+print(a);
+
+c=10;
+b = int((c==y) or (y>z));
+print(b);
+
+b= int((c>y) and (z>0));
+print(b);
