e2a6a911b587619f0b75aab0f5629de9361b4095
Anson avarachan <anson.mec@gmail.com>
2023-11-13 10:45:51 +0530

    Update TEST.py

--- compiler/tests/TEST.py
+++ compiler/tests/TEST.py
@@ -1,6 +1,18 @@
-
 import os
-os.system('python test1.py > o')
-os.system('python ../main.py test1.an -sim > o1')
-os.system('python ../main.py test1.an -compile > o2')
-print(open('o','r').read() == open('o1','r').read() == open('o2','r').read())
+os.system('python test_binary.py > o')
+os.system('python ../main.py test_binary.an -sim > o1')
+os.system('python ../main.py test_binary.an -compile > o2')
+print('Testing binary ops -> ',open('o','r').read() == open('o1','r').read() == open('o2','r').read())
+
+
+os.system('python test_relational_equality.py > o')
+os.system('python ../main.py test_relational_equality.an -sim > o1')
+os.system('python ../main.py test_relational_equality.an -compile > o2')
+print('Testing relational equality ops -> ', open('o','r').read() == open('o1','r').read() == open('o2','r').read())
+
+os.remove('hello')
+os.remove('hello.o')
+os.remove('o')
+os.remove('o1')
+os.remove('o2')
+os.remove('compile.asm')
