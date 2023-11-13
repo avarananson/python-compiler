143175321cf0f4037bf27793c94bd29b6274aa00
Anson avarachan <anson.mec@gmail.com>
2023-11-13 13:37:58 +0530

    Update TEST.py

--- compiler/tests/TEST.py
+++ compiler/tests/TEST.py
@@ -1,4 +1,5 @@
 import os
+print('========================================================================')
 os.system('python test_binary.py > o')
 os.system('python ../main.py test_binary.an -sim > o1')
 os.system('python ../main.py test_binary.an -compile > o2')
@@ -10,9 +11,15 @@ os.system('python ../main.py test_relational_equality.an -sim > o1')
 os.system('python ../main.py test_relational_equality.an -compile > o2')
 print('Testing relational equality ops -> ', open('o','r').read() == open('o1','r').read() == open('o2','r').read())
 
+os.system('python test_logical.py > o')
+os.system('python ../main.py test_logical.an -sim > o1')
+os.system('python ../main.py test_logical.an -compile > o2')
+print('Testing logical equality ops -> ', open('o','r').read() == open('o1','r').read() == open('o2','r').read())
+
 os.remove('hello')
 os.remove('hello.o')
 os.remove('o')
 os.remove('o1')
 os.remove('o2')
 os.remove('compile.asm')
+print('========================================================================')
